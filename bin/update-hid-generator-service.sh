7beccf7ac2ec610694a6719930db2fca9cf7b83a
Ron Kurr <kurr@kurron.org>
2015-12-23 22:09:56 +0000

    Improved the update mechanism

--- bin/update-hid-generator-service.sh
+++ bin/update-hid-generator-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CMD='/usr/local/bin/aws ecs update-service --cli-input-json file://$PWD/service-definitions/asgard-hid-generator.json'
+CMD='/usr/local/bin/aws ecs update-service --cli-input-json file://$PWD/service-definitions/asgard-hid-generator-update.json'
 echo $CMD
 $CMD
